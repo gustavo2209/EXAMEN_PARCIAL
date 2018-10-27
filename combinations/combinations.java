979c3c50d535e06960ed3a16d573a6daac1820b2
Gustavo Chimoy García <gchimoygarcia22@gmail.com>
2018-10-27 09:51:29 -0500

    Update combinations.java

--- combinations/combinations.java
+++ combinations/combinations.java
@@ -1,1 +1,45 @@
+public class Combinations {
 
+    public static void main(String[] args) {
+        
+        Combinations combinations = new Combinations();
+	      int n = 5;
+	      System.out.println("# Ladder's steps: " + n);
+	      System.out.println("# Step combinations: " + combinations.countCombinations(n, 0));
+        System.out.println("# Step combinations: " + combinations.countCombinationsOptimize(n, 0));
+        
+    }
+    
+    public long countCombinations(int n, int level) {
+        
+        if (n < 0) {
+            return 0;
+	      }
+        if (n == 0 && level == 0) {
+                  return 0;
+        }
+        if (n == 0 && level > 0) {
+                  return 1;
+        }
+      
+	      return countCombinations(n - 1, level + 1) + countCombinations(n - 2, level + 1) + countCombinations(n - 3, level + 1);
+	
+    }
+    
+    public long countCombinationsOptimize(int n, int level) {
+
+        if (n < 0) {
+            return 0;
+	      }
+        if (n == 0 && level == 0) {
+                  return 0;
+        }
+        if (n == 0 && level > 0) {
+                  return 1;
+        }
+	      
+        return countCombinations(n - 1, level + 1) + countCombinations(n - 2, level + 1) + countCombinations(n - 3, level + 1);
+	
+    }
+    
+}
